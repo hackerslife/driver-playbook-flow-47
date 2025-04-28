e699ffaecca4674804b0cda9662bafe525c3d6d7
hackerslife <akanshashrm284@gmail.com>
2025-04-28 16:24:27 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -14,7 +14,7 @@ const Index = () => {
         </p>
         <Button 
           className="bg-blue-600 hover:bg-blue-700 text-white px-8 py-6 text-lg rounded-lg shadow-lg transition-all"
-          onClick={() => navigate("/tracker")}
+          onClick={() => navigate("/playbook")}
         >
           Go to Playbook Tracker
         </Button>
